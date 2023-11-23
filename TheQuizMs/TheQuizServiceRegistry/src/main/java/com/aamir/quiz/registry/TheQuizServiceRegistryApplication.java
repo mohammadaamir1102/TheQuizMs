d9e1d473a787a165d621c68c79c49f660885aa0b
mohammadaamir1102 <mohammadaamir1102@gmail.com>
2023-11-23 12:31:54 +0530

    enable eureka server

--- TheQuizMs/TheQuizServiceRegistry/src/main/java/com/aamir/quiz/registry/TheQuizServiceRegistryApplication.java
+++ TheQuizMs/TheQuizServiceRegistry/src/main/java/com/aamir/quiz/registry/TheQuizServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.aamir.quiz.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TheQuizServiceRegistryApplication {
 
 	public static void main(String[] args) {
